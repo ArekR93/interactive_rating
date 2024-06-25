684a7b353150cdfa974a37c3367d1183761e44b2
Arkadiusz <arkadiuszruszczak0@gmail.com>
2024-06-25 18:24:13 +0200

    finished project

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const btns = document.querySelectorAll('.btn-choice')
+    const userChoice = document.querySelector('.user-value')
+    const submitBtn = document.querySelector('.submit-btn')
+    const ratingState = document.querySelector('.rating-state')
+    const thankYou = document.querySelector('.thank-you')
 
+    const takenButton = (e) => {
+        button = e.target
+        button.classList.toggle('taken')
+        const btnChoice = button.innerHTML;
+        userChoice.innerHTML = btnChoice
+
+    }
+    const changePanel = () => {
+        if(ratingState.classList.contains('active')){
+            ratingState.classList.remove('active')
+            thankYou.classList.add('active')
+        }
+    }
+
+    btns.forEach((button) => {
+        button.addEventListener('click', takenButton)
+    })
+
+    submitBtn.addEventListener('click',changePanel)
 
 })
